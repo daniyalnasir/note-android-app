401fc848233fbf5293bd675a565d89dfab59fb8e
Daniyal Nasir <syeddaniyalnasir@hotmail.com>
2022-09-22 17:39:38 -0400

    AN-0005 - Setup Room database

---
+++ app/src/main/java/com/example/noteapp/database/NotesDatabase.kt
@@ -0,0 +1,19 @@
+package com.example.noteapp.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.noteapp.feature_note.data.data_sources.NoteDao
+import com.example.noteapp.feature_note.domain.models.NoteModel
+
+@Database(
+    entities = [NoteModel::class],
+    version = 1
+)
+abstract class NotesDatabase: RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
+}
--- app/src/main/java/com/example/noteapp/feature_note/domain/models/NoteModel.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/models/NoteModel.kt
@@ -3,7 +3,7 @@ package com.example.noteapp.feature_note.domain.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "Note")
 data class NoteModel(
     @PrimaryKey val id: Int? = null,
     val title: String,
